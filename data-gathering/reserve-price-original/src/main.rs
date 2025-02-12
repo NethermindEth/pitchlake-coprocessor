3a8249b08447e0f5c636f5a9bbe89e49dd5f2874
Chee Chyuan Ang <cheechyuanang@Chees-MacBook-Pro-2.local>
2025-02-12 17:40:07 +0800

    slight modify, not essential for main operation, merely for ease of data gathering

--- data-gathering/reserve-price-original/src/main.rs
+++ data-gathering/reserve-price-original/src/main.rs
@@ -8,11 +8,21 @@ use reserve_price_original::calculate_reserve_price;
 async fn main() {
     let df = read_data_from_file("data.csv");
     let df = add_df_property(df);
-    let periods = split_dataframe_into_periods(df, 3).unwrap();
-    let max_iterations = 10000;
+    let periods: Vec<polars::prelude::DataFrame> = split_dataframe_into_periods(df, 3).unwrap();
+    // let max_iterations = 25000;
+    // let period_index = 5;
+
+    // println!(
+    //     "period_index: {}, max_iterations: {}",
+    //     period_index, max_iterations
+    // );
 
     let mut wtr = open_reserve_price_csv_writer("reserve_price_reserve_price_original.csv");
-    for period in periods {
+    for (i, period) in periods.iter().enumerate() {
+        // if i != period_index {
+        //     continue;
+        // }
+
         let reserve_price = calculate_reserve_price(period.clone(), max_iterations).await;
         if reserve_price.is_err() {
             continue;
@@ -42,10 +52,3 @@ async fn main() {
     }
     close_csv_file(&mut wtr);
 }
-
-// Solution { position: [-0.017633342845010558, 0.8891236203630338, 0.04937254907891192, 0.01382943862840793, 0.15257919054410768, 0.3365041893564977], value: -189.01247078357096 } 2400 iter
-// Solution { position: [-0.01762796744188326, 0.8891310386302813, 0.04932426869529885, 0.013824002748472133, 0.1525309538232292, 0.33666040650388845], value: -189.01249239179717 } 3500 iter
-// Solution { position: [-0.01762748615163198, 0.8891317028744364, 0.04931994818663203, 0.01382354520544229, 0.1525266305724204, 0.33667441434122203], value: -189.0124925657238 } 5000 iter
-// Solution { position: [-0.01762748615163198, 0.8891317028744364, 0.04931994818663203, 0.01382354520544229, 0.1525266305724204, 0.33667441434122203], value: -189.0124925657238 } 5500 iter
-// Solution { position: [-0.01762748615163198, 0.8891317028744364, 0.04931994818663203, 0.01382354520544229, 0.1525266305724204, 0.33667441434122203], value: -189.0124925657238 } 8000 iter
-// Solution { position: [-0.01762748615163198, 0.8891317028744364, 0.04931994818663203, 0.01382354520544229, 0.1525266305724204, 0.33667441434122203], value: -189.0124925657238 } 10,000 iter
