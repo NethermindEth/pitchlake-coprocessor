9a6c61c8c5b30e2970d07d5611e551681a46d8e6
Chee Chyuan Ang <cheechyuanang@Chees-MacBook-Pro-2.local>
2025-03-17 14:55:27 +0800

    delete unused methods

--- benchmark-risc0/calculate-reserve-price-from-simulated-log-prices-floating/core/src/lib.rs
+++
@@ -1,9 +0,0 @@
-use nalgebra::DMatrix;
-use serde::{Deserialize, Serialize};
-
-#[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
-pub struct CalculateReservePriceFromSimulatedLogPricesInput {
-    pub simulated_log_prices: DMatrix<f64>,
-    pub twap_7d: Vec<f64>,
-    pub n_periods: usize,
-}
--- benchmark-risc0/simulate-price-fixed/README.md
+++
@@ -1,5 +0,0 @@
-# This is currently not working
-
-Using fixed point, we are seeing that there is a division by zero error in our `minimize` function. This error is not present in our floating point implementation. 
-
-To fix this if we need fixed point optimization in the other PRs
